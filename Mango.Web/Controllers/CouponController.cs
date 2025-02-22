d6f8d2900ce2aba3cdde7b7887640118d8ca72dd
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-02-22 23:06:04 +0200

    add view controller for coupon

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,50 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+
+            _couponService=couponService;
+
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto?> list = new();
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+            
+            if (response != null && response.IsSuccess)
+            {
+                list=JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid) 
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+    }
+}
