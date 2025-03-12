cc292190e5c531d9f035ad5b43510319e31ab79c
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-12 18:03:53 +0200

    create Cart API with all Dtos

---
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string? UserID { get; set; }
+        public string? CouponCode { get; set; }
+
+        [NotMapped]
+        public double Discount { get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+
+
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        
+        public int CartHeaderId { get; set; }
+        public string? UserID { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+
+
+    }
+}
+}
