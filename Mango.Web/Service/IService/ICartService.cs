fb44590d4fdbe10070e7c2bd8d89cb20662b16cf
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-14 15:46:09 +0200

    add cart service to web

---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+
+
+    }
+}
