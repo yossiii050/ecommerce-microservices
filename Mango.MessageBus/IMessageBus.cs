20ee354de87d5426e93f225ff4c06c502c9414fc
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-15 17:20:15 +0200

    Removed secret from code

---
+++ Mango.MessageBus/IMessageBus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task PublicMessage(object message, string topic_queue_Name);
+        
+    }
+}
