79aefbaf369f741be64e48036a0f080643db3292
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-23 19:00:36 +0200

    Configure Database Endpoint

--- Mango.Web/Service/IService/IOrderService.cs
+++ Mango.Web/Service/IService/IOrderService.cs
@@ -10,6 +10,9 @@ namespace Mango.Web.Service.IService
         Task<ResponseDto?> CreateStripeSession(StripeRequestDto stripeRequestDto);
 
         Task<ResponseDto?> ValidateStripeSession(int orderHeaderId);
+        Task<ResponseDto?> GetAllOrder(string? userId);
+        Task<ResponseDto?> GetOrder(int orderId);
+        Task<ResponseDto?> UpdateOrderStatus(int orderId,string newStatus);
 
     }
 }
