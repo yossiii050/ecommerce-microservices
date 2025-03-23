7b9665a6d6c45cd4f87b2b74311d56de2c041700
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-23 16:48:50 +0200

    create rewards API

---
+++ Mango.Services.RewardAPI/Models/Rewards.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.RewardAPI.Models
+{
+    public class Rewards
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime RewardDate { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+
+    }
+}
