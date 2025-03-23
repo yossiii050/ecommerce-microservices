192f214e50007bbb411650dad0e2e14523e8a83f
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-03-23 19:58:52 +0200

    Order Management API with Refunds

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utillity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
@@ -20,8 +21,66 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        public async Task<IActionResult> OrderDetail(int orderId)
+        {
+            OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
+            string userId=User.Claims.Where(u=>u.Type== JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+
+            var response = await _orderService.GetOrder(orderId);
+            if (response != null && response.IsSuccess)
+            {
+                orderHeaderDto=JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+            }
+            if(!User.IsInRole(SD.RoleAdmin)&& userId!= orderHeaderDto.UserID)
+            {
+                return NotFound();
+            }
+            return View(orderHeaderDto);
+        }
+
+        [HttpPost("OrderReadyForPickup")]
+        public async Task<IActionResult> OrderReadyForPickup(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId,SD.Status_ReadyForPickup);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"]="Status Updated successfully";
+                return RedirectToAction(nameof(OrderDetail),new {OrderId=orderId});
+            }
+
+            return View();
+        }
+
+
+        [HttpPost("CompleteOrder")]
+        public async Task<IActionResult> CompleteOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Completed);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"]="Status Updated successfully";
+                return RedirectToAction(nameof(OrderDetail), new { OrderId = orderId });
+            }
+
+            return View();
+        }
+
+
+        [HttpPost("CancelOrder")]
+        public async Task<IActionResult> CancelOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Cancelled);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"]="Status Updated successfully";
+                return RedirectToAction(nameof(OrderDetail), new { OrderId = orderId });
+            }
+
+            return View();
+        }
+
         [HttpGet]
-        public IActionResult GetAll() 
+        public IActionResult GetAll(string status) 
         {
             IEnumerable<OrderHeaderDto> list ;
             string userId = "";
@@ -34,6 +93,21 @@ namespace Mango.Web.Controllers
             if(response != null && response.IsSuccess)
             {
                 list=JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+                switch(status) 
+                { 
+                    case "approved":
+                        list=list.Where(u => u.Status == SD.Status_Approved);
+                        break;
+					case "readyforpickup":
+						list=list.Where(u => u.Status == SD.Status_ReadyForPickup);
+						break;
+					case "cancelled":
+						list=list.Where(u => u.Status == SD.Status_Cancelled);
+						break;
+                    default:
+                        break;
+
+				}
             }
             else
             {
