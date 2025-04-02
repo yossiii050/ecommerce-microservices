1b5270cbbbbab5f6e08e98c500314761f3d67416
yossiii050 <78302727+yossiii050@users.noreply.github.com>
2025-04-02 16:07:32 +0300

    Ocelot endpoints

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,6 +1,16 @@
+using Mango.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthetication();
+builder.Services.AddOcelot();
+
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
